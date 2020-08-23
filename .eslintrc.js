cd810c1e2e128c283615792aef7d4526df185562
Nop Jiarathanakul <nop@iamnop.com>
2020-08-23 13:03:11 -0700

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  rules: {
+    curly: ["error", "all"],
+    "no-unused-vars": ["error", { args: "none" }],
+    "no-underscore-dangle": "off",
+    "no-plusplus": "off",
+    "no-param-reassign": "off",
+    "prefer-destructuring": "off",
+    "import/no-commonjs": "error",
+    "react/destructuring-assignment": "off",
+    "react/prop-types": "off",
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="canvas" />
+      <div className="page" />
+    </div>
+  );
+}
+
+export default App;
