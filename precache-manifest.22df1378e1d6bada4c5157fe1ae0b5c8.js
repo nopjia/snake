5d8362ee81ab712e5cb9bd31a6c69fd5b9fc78d0
Nop Jiarathanakul <nop@iamnop.com>
2020-09-06 13:35:50 -0700

    Updates

---
+++ precache-manifest.22df1378e1d6bada4c5157fe1ae0b5c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34641686789667d9009bd51253df465",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "92e25951fdfa5d16be52",
+    "url": "/snake/static/css/main.3e241785.chunk.css"
+  },
+  {
+    "revision": "442430adc3e561ff801e",
+    "url": "/snake/static/js/2.c2e6800e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/snake/static/js/2.c2e6800e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e25951fdfa5d16be52",
+    "url": "/snake/static/js/main.fb068ff8.chunk.js"
+  },
+  {
+    "revision": "5deeaf6e2dd8dea0308797cfe0ec9ff0",
+    "url": "/snake/static/js/main.fb068ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "270441e474012128cff0",
+    "url": "/snake/static/js/runtime-main.30d4ce42.js"
+  }
+]);
