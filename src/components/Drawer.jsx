607c617ed52a0a62eecbdb47b42d3347953288fa
Nop Jiarathanakul <nop@iamnop.com>
2020-09-07 18:59:35 -0700

    add tooltips to fab icons

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Fab, Icon } from "@material-ui/core";
+import { Fab, Icon, Tooltip } from "@material-ui/core";
 
 const Drawer = (props) => {
   const style = {};
@@ -10,34 +10,50 @@ const Drawer = (props) => {
     <div className="drawer" style={style}>
       <div className="bar">
         <div className="left">
-          <Fab onClick={props.onReset}>
-            <Icon>refresh</Icon>
-          </Fab>
-          <Fab onClick={props.onFocus}>
-            <Icon>center_focus_strong</Icon>
-          </Fab>
+          <Tooltip title="Reset Snake">
+            <Fab onClick={props.onReset}>
+              <Icon>refresh</Icon>
+            </Fab>
+          </Tooltip>
+          <Tooltip title="Center Camera">
+            <Fab onClick={props.onFocus}>
+              <Icon>center_focus_strong</Icon>
+            </Fab>
+          </Tooltip>
         </div>
         <div className="center">
-          <Fab onClick={props.onMoveLeft}>
-            <Icon>arrow_backward</Icon>
-          </Fab>
-          <Fab onClick={props.onRotateLeft}>
-            <Icon>undo</Icon>
-          </Fab>
-          <Fab onClick={props.onRotateRight}>
-            <Icon>redo</Icon>
-          </Fab>
-          <Fab onClick={props.onMoveRight}>
-            <Icon>arrow_forward</Icon>
-          </Fab>
+          <Tooltip title="Previous Block">
+            <Fab onClick={props.onMoveLeft}>
+              <Icon>arrow_backward</Icon>
+            </Fab>
+          </Tooltip>
+          <Tooltip title="Rotate">
+            <Fab onClick={props.onRotateLeft}>
+              <Icon>undo</Icon>
+            </Fab>
+          </Tooltip>
+          <Tooltip title="Rotate">
+            <Fab onClick={props.onRotateRight}>
+              <Icon>redo</Icon>
+            </Fab>
+          </Tooltip>
+          <Tooltip title="Next Block">
+            <Fab onClick={props.onMoveRight}>
+              <Icon>arrow_forward</Icon>
+            </Fab>
+          </Tooltip>
         </div>
         <div className="right">
-          <Fab onClick={props.onCamera}>
-            <Icon>camera_alt</Icon>
-          </Fab>
-          <Fab onClick={props.onToggle}>
-            <Icon>{props.opened ? "close" : "collections"}</Icon>
-          </Fab>
+          <Tooltip title="Save Shape">
+            <Fab onClick={props.onCamera}>
+              <Icon>camera_alt</Icon>
+            </Fab>
+          </Tooltip>
+          <Tooltip title={props.opened ? "Close" : "Open Gallery"}>
+            <Fab onClick={props.onToggle}>
+              <Icon>{props.opened ? "close" : "collections"}</Icon>
+            </Fab>
+          </Tooltip>
         </div>
       </div>
       <div className="content">{props.children}</div>
