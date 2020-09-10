b49cea96e9d0927f8ed60b67c312a3569332a66e
Nop Jiarathanakul <nop@iamnop.com>
2020-09-09 23:53:04 -0700

    get rid of standard shapes

--- src/api.js
+++ src/api.js
@@ -1,22 +1,10 @@
 import axios from "axios";
 
-const _PUBLIC_URL = process.env.PUBLIC_URL;
 const _API_URL = process.env.REACT_APP_API_URL;
-
-let _standardShapes;
-let _standardSequenceMap;
-
-const getStandardShapes = async () => {
-  if (!_standardShapes) {
-    const res = await axios.get(`${_PUBLIC_URL}/standardShapes.json`);
-    _standardShapes = res.data;
-  }
-  return _standardShapes;
-};
 
 const getShapes = async (offset = 0, limit = 15) => {
   const filter = {
-    // order: ["created ASC viewCount DESC"],
+    // order: ["created ASC"],
     order: ["viewCount DESC"],
     offset,
     limit,
@@ -38,32 +26,11 @@ const incrementViewCount = async (id) => {
   return axios.patch(`${_API_URL}/shapes/incrementViewCount/${id}`);
 };
 
-const _getStandardSequenceMap = () => {
-  if (!_standardSequenceMap) {
-    _standardSequenceMap = {};
-    _standardShapes.forEach((s) => {
-      _standardSequenceMap[s.sequence] = s;
-    });
-  }
-  return _standardSequenceMap;
-};
-
 const hasSequence = async (sequence) => {
-  const standardMap = _getStandardSequenceMap();
-  if (standardMap[sequence]) {
-    return true;
-  }
-
   const res = await axios.get(
     `${process.env.REACT_APP_API_URL}/shapes/hasSequence/${sequence}`
   );
   return res.data;
 };
 
-export {
-  getStandardShapes,
-  getShapes,
-  postShape,
-  incrementViewCount,
-  hasSequence,
-};
+export { getShapes, postShape, incrementViewCount, hasSequence };
